4639edc937ec3e599eaddc59b8285bf4856552fa
Anastasia <dzheyn.anna@mail.ru>
2023-09-14 22:51:27 +0400

    minor

--- frontend/src/components/breadcrumbs/breadcrumbs.tsx
+++ frontend/src/components/breadcrumbs/breadcrumbs.tsx
@@ -1,15 +1,29 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import { AppRoute } from '../../utils/constant';
 
 function Breadcrumbs(): JSX.Element {
+  const { pathname } = useLocation();
+
   return (
     <div>
       <ul className="breadcrumbs page-content__breadcrumbs">
-        <li className="breadcrumbs__item"><Link className="link" to="./main.html">Главная</Link>
+        <li className="breadcrumbs__item">
+          <Link className="link" to={AppRoute.Login}>
+            Главная
+          </Link>
         </li>
-        <li className="breadcrumbs__item"><Link className="link" to="./main.html">Каталог</Link>
+        <li className="breadcrumbs__item">
+          <Link className="link" to={AppRoute.List}>
+            Каталог
+          </Link>
         </li>
-        <li className="breadcrumbs__item"><Link className="link" to=''>Товар</Link>
-        </li>
+        {pathname !== AppRoute.List && (
+          <li className="breadcrumbs__item">
+            <Link className="link" to={pathname}>
+              Товар
+            </Link>
+          </li>
+        )}
       </ul>
     </div>
   );
--- frontend/src/components/guitar-card/guitar-card-small.tsx
+++ frontend/src/components/guitar-card/guitar-card-small.tsx
@@ -1,4 +1,8 @@
+import { Link } from 'react-router-dom';
+import { AppRoute, FormStatus } from '../../utils/constant';
+
 const mockGuitar = {
+  id:1,
   name: 'СURT Z30 Plus',
   type:'Электрогитара',
   price:17500,
@@ -10,13 +14,13 @@ function GuitarCardSmall(): JSX.Element {
     <>
       <div className="catalog-item__data"><img src="img/content/catalog-product-1.png" srcSet="img/content/catalog-product-1@2x.png 2x" width="36" height="93" alt="Картинка гитары"/>
         <div className="catalog-item__data-wrapper">
-          <a className="link" href="./product.html"><p className="catalog-item__data-title">{mockGuitar.type} {mockGuitar.name}</p></a>
+          <Link className="link" to={`${AppRoute.List}/1`}><p className="catalog-item__data-title">{mockGuitar.type} {mockGuitar.name}</p></Link>
           <br/>
           <p className="catalog-item__data-date">Дата добавления {mockGuitar.date}</p>
           <p className="catalog-item__data-price">{mockGuitar.price} ₽</p>
         </div>
       </div>
-      <div className="catalog-item__buttons"><a className="button button--small button--black-border" href="edit-item.html" aria-label="Редактировать товар">Редактировать</a>
+      <div className="catalog-item__buttons"><Link className="button button--small button--black-border" to={`${AppRoute.List}/${mockGuitar.id}/${FormStatus.Edit}`} aria-label="Редактировать товар">Редактировать</Link>
         <button className="button button--small button--black-border" type="submit" aria-label="Удалить товар">Удалить</button>
       </div>
     </>
